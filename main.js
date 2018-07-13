45bef60dfca68a9f5f83f4f2604be0924a4fb201
zhengbigbig <git@github.com>
2018-07-13 10:56:36 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,65 @@
+let $buttons = $('#bottomBar > li')
+let $tags = $('.main > section')
+function watchTouchevent(){
+  // $([document.documentElement, document.body]).animate({
+  //   scrollTop: $tags.eq(0).offset().top
+  // }, 1000)
+  $buttons.on('touchstart',function(e){
+     var index = $(e.currentTarget).index()
+     $buttons.removeClass('active')
+     $buttons.eq(index).addClass('active')
+     $([document.documentElement, document.body]).animate({
+      scrollTop: $tags.eq(index).offset().top
+
+  }, 1000)
+  })
+  
+  $($tags).on('touchmove',function(e){
+    var index = $(e.currentTarget).index()
+    $buttons.removeClass('active')
+    $buttons.eq(index).addClass('active')
+    $('#bottomBar').addClass('stiky') 
+    $('#bottomBar > li').addClass('highlight')
+  })
+  $($tags).on('touchend',function(){
+    setTimeout(()=>{
+      $('#bottomBar').removeClass('stiky')
+      $('#bottomBar > li').removeClass('highlight')
+    },3000)  
+  })
+  $('#hoverme').on('touchstart',function (){
+    $('#hoverme').addClass('hover') 
+  })
+  $('#hoverme').on('touchend',function (){
+    $('#hoverme').removeClass('hover') 
+  })
+}
+function watchMouseevent (){
+  // $([document.documentElement, document.body]).animate({
+  //   scrollTop: $tags.eq(0).offset().top
+  // }, 1000)
+  $buttons.on('click',function(e){
+     var index = $(e.currentTarget).index()
+     $buttons.removeClass('active')
+     $buttons.eq(index).addClass('active')
+     $([document.documentElement, document.body]).animate({
+      scrollTop: $tags.eq(index).offset().top
+  }, 1000)
+  })
+  $($tags).on('mouseenter',function(e){
+    var index = $(e.currentTarget).index()
+    $buttons.removeClass('active')
+    $buttons.eq(index).addClass('active')
+  })
+  $('.main').on('scroll',function(e){
+    console.log(1)
+    console.log(e.scrollY)
+  })
+
+}
+
+if(document.ontouchstart !== undefined){
+  watchTouchevent()
+}else{
+  watchMouseevent ()
+}
